e2ceea3f3a5d6acc31ca0feee2c23a1620e5751c
Gopal Rao Pulluri <Gopal.Pulluri@ATMECS.CORP>
2020-10-21 12:48:46 +0530

    Added the root url

--- src/main/java/com/example/awscicddemo/HelloWorldController.java
+++ src/main/java/com/example/awscicddemo/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/cicd")
 public class HelloWorldController{
 
     @GetMapping(value = "/health")
