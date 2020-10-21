0de31ad1659e22093011d9509f0431af0653986b
Gopal.Pulluri <abc@abc.com>
2020-10-21 14:13:28 +0530

    Added the missed import

--- src/main/java/com/example/awscicddemo/HelloWorldController.java
+++ src/main/java/com/example/awscicddemo/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.awscicddemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RestController
